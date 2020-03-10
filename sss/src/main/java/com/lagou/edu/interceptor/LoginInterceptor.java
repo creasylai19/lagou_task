9bb1be781aeaeb4caca6b6df4e3e9930a1fb90a2
creasylai19 <creasylai19@gmail.com>
2020-03-10 14:21:54 +0800

    提交阶段一模块三作业二

---
+++ sss/src/main/java/com/lagou/edu/interceptor/LoginInterceptor.java
@@ -0,0 +1,21 @@
+package com.lagou.edu.interceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class LoginInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        HttpSession session = request.getSession();
+        Object login = session.getAttribute("login");
+        if(null != login && ((boolean)login)) {
+            return true;
+        }
+        request.setAttribute("message", "您未登录，请先登录");
+        request.getRequestDispatcher(request.getContextPath() + "/index.jsp").forward(request, response);
+        return false;
+    }
+}
